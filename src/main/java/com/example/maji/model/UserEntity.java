f94269ae08484aa79f96623991fe41ff5412652c
WonilMon <ll350ll@naver.com>
2024-11-04 16:52:21 +0900

    241104_test_wonil

--- src/main/java/com/example/maji/model/UserEntity.java
+++ src/main/java/com/example/maji/model/UserEntity.java
@@ -7,20 +7,52 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name = "test_table")
+@Table(name = "users")
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "user_idx")
     private Long userIdx;
 
-//  @Column(name = "userName", nullable = false, length = 50, unique = true)
-    private String userName;
+    @Column(name = "user_fn")
+    private String userFn;
 
+    @Column(name = "user_fny")
+    private String userFny;
+
+    @Column(name = "user_ln")
+    private String userLn;
+
+    @Column(name = "user_lny")
+    private String userLny;
+
+    @Column(name = "user_id")
     private String userId;
 
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "user_pass")
     private String userPass;
 
-    private String userType;
+    @Column(name = "user_age")
+    private String userAge;
+
+    @Column(name = "user_date")
+    private String userDate;
+
+    @Column(name = "user_gender")
+    private String userGender;
+
+    @Column(name = "user_img")
+    private String userImg;
+
+    @Column(name = "user_role")
+    private String userRole;
+
+    @Column(name = "user_point")
+    private String userPoint;
+
 
 }
--- src/main/java/com/example/maji/service/UserService.java
+++ src/main/java/com/example/maji/service/UserService.java
@@ -1,9 +1,12 @@
 package com.example.maji.service;
 
+import com.example.maji.bean.UserBean;
 import com.example.maji.model.UserEntity;
 import com.example.maji.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -11,10 +14,31 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public UserEntity saveName(String name) {
-        UserEntity entity = new UserEntity();
-        entity.setUserName(name);
-        return userRepository.save(entity);
+    //--------------------------------------------
+
+    //회원가입
+    public UserEntity joinUser(UserBean joinUserBean) {
+
+        UserEntity userEntity = new UserEntity();
+
+        userEntity.setUserId(joinUserBean.getUserId());
+        userEntity.setUserPass(joinUserBean.getUserPass());
+        userEntity.setUserFn(joinUserBean.getUserFn());
+        userEntity.setUserFny(joinUserBean.getUserFny());
+        userEntity.setUserLn(joinUserBean.getUserLn());
+        userEntity.setUserLny(joinUserBean.getUserLny());
+        userEntity.setUserEmail(joinUserBean.getUserEmail());
+
+        return userRepository.save(userEntity);
+
+    }
+
+    //로그인
+    public boolean authenticate(String userId, String userPass) {
+        Optional<UserEntity> user = userRepository.findByUserId(userId);
+
+        return user.isPresent() && user.get().getUserPass().equals(userPass);
+
     }
 
 }
